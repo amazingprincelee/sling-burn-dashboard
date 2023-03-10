281871361b1b1730a16db39ff845ec95f154e28d
eddiebee <edbeeotudor@gmail.com>
2023-03-10 09:10:33 +0000

    Install packages to container and refactor imports

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -2,9 +2,8 @@ import moment from 'moment/moment';
 import React, { useState, useEffect} from 'react';
 import DataTable from 'react-data-table-component';
 import { tableCustomStyles } from '../tableStyle';
-
-const Converter = require('timestamp-conv');
-const convert = require('ethereum-unit-converter')
+import Converter from 'timestamp-conv'
+import convert from 'ethereum-unit-converter'
 
 
 const transactionApi = 'https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0x5a79be6cdce26bc853d72919bf98a0378641b607&address=0x000000000000000000000000000000000000dEaD&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=RM1PBIHRH9F5JVJYH7ENZI6SWZMGXU3FTY';
