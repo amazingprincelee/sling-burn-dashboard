77a89819358b7c74da2a90be84044ae7d5ea8669
prince Lee <princeleepraise@gmail.com>
2023-03-10 08:44:45 +0100

    Add pakage ethereum converter

---
+++ src/components/SupplyDisplay.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const SupplyDisplay = () => {
+    return (
+        <div className='supply-display'>
+
+            <p></p>
+            
+        </div>
+    );
+};
+
+
+export default SupplyDisplay;
--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -4,6 +4,7 @@ import DataTable from 'react-data-table-component';
 import { tableCustomStyles } from '../tableStyle';
 
 const Converter = require('timestamp-conv');
+const convert = require('ethereum-unit-converter')
 
 
 const transactionApi = 'https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0x5a79be6cdce26bc853d72919bf98a0378641b607&address=0x000000000000000000000000000000000000dEaD&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=RM1PBIHRH9F5JVJYH7ENZI6SWZMGXU3FTY';
@@ -55,9 +56,12 @@ const Tables = (props) => {
                 const newApidata = apiData.map((data) => {
                 const Date = new Converter.date(data.timeStamp);
 
-            //value conversion    
-            const value = Number(data.value) / 1000000000000000;
-            const slingValue = value.toFixed(0);
+            //value conversion 
+               
+            
+            const slingValue = convert(data.value, 'wei', 'ether').toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+            
 
                     
                     
