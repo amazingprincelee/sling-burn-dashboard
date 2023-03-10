1156db68a8e28a065c5811593f4b9bbc7aebad7a
eddiebee <edbeeotudor@gmail.com>
2023-03-10 10:28:52 +0000

    Reuse Tables component as necessary

--- src/pages/BurnHistory.jsx
+++ src/pages/BurnHistory.jsx
@@ -1,84 +1,34 @@
-import React, { useState, useEffect} from 'react';
-import DataTable from 'react-data-table-component';
-import { tableCustomStyles } from '../tableStyle';
+import React, { useState } from 'react';
 import LineChart from '../components/LineChart';
 import { SlingData } from "../Data";
+import Tables from '../components/Tables';
 
 
 const BurnHistory = () => {
+    const [slingData, setSlingData] = useState({
+        labels: SlingData.map((data) => data.percentage),
 
-    const [tableData, setTableData] = useState()
-
-    const columns = [
-        {
-            name: 'Time',
-            selector: row => row.timeStamp
-            
-        },
-
-        {
-            name: 'From',
-            selector: row => row.from
-        },
-
-        {
-            name: 'To',
-            selector: row => row.to
-        },
-
-        {
-            name: '$Sling',
-            selector: row => '1'
-        }
-    ];
-
-    useEffect(() => {
-        fetch('https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0x5a79be6cdce26bc853d72919bf98a0378641b607&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=RM1PBIHRH9F5JVJYH7ENZI6SWZMGXU3FTY')
-           .then((response) => response.json())
-           .then((response) => {
-              setTableData(response.result);
-              
-
-            
-           })
-           .catch((err) => {
-              console.log(err.message);
-           });
-     }, []);
-
-
-
-        const [slingData, setSlingData] = useState({
-            labels: SlingData.map((data) => data.percentage),
-
-            datasets: [
-                {
-                    label: "Burn Gained",
-                    data: SlingData.map((data) => data.burntAmount),
-                    backgroundColor: ["red"]
-                }
-            ]
-        })
+        datasets: [
+            {
+                label: "Burn Gained",
+                data: SlingData.map((data) => data.burntAmount),
+                backgroundColor: ["red"]
+            }
+        ]
+    })
 
     return (
         <div>
             <h1>Burn History</h1>
             <div className='container center'>
-              <div style={{width: 700 }}>
-                <LineChart 
-                    chartData={slingData}
-                />
-              </div>  
+                <div style={{ width: 700 }}>
+                    <LineChart
+                        chartData={slingData}
+                    />
+                </div>
             </div>
-            <h3>Burn Transactions</h3>
-            <div className='data-table-history'>
-            
-                <DataTable columns={columns}
-                    data={tableData}
-                    fixedHeader
-                    pagination
-                    customStyles={tableCustomStyles}
-                 />      
+            <div>
+                <Tables tableTitle={'Burn Transactions'} tableClass={'data-table-history'} pagination={true} />
             </div>
         </div>
     );
--- src/pages/TopBurners.jsx
+++ src/pages/TopBurners.jsx
@@ -1,62 +1,12 @@
-import React, { useState, useEffect} from 'react';
-import DataTable from 'react-data-table-component';
-import { tableCustomStyles } from '../tableStyle';
-
+import React from 'react';
+import Tables from '../components/Tables';
 
 const TopBurners = () => {
-
-    const [tableData, setTableData] = useState()
-
-    const columns = [
-        {
-            name: 'Time',
-            selector: row => row.timeStamp
-            
-        },
-
-        {
-            name: 'From',
-            selector: row => row.from
-        },
-
-        {
-            name: 'To',
-            selector: row => row.to
-        },
-
-        {
-            name: '$Sling',
-            selector: row => "1"
-        }
-    ];
-
-    useEffect(() => {
-        fetch('https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0x5a79be6cdce26bc853d72919bf98a0378641b607&address=0x000000000000000000000000000000000000dEaD&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=2KBH34YNQ4QHV4E5RHF5Y4QBCDWG5RJ5IP')
-           .then((response) => response.json())
-           .then((response) => {
-              setTableData(response.result);
-              
-
-            
-           })
-           .catch((err) => {
-              console.log(err.message);
-           });
-     }, []);
 
     return (
         <div>
             <h1>Top Burners</h1>
-            <h3>Burn Ranking</h3>
-            <div className='data-table-history'>
-            
-                <DataTable columns={columns}
-                    data={tableData}
-                    fixedHeader
-                    pagination
-                    customStyles={tableCustomStyles}
-                 />      
-            </div>
+            <Tables tableTitle={'Burn Ranking'} tableClass={'data-table-history'} pagination={true} />
         </div>
     );
 };
