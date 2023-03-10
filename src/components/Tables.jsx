2c3e3c5aa73e96b931fce749d4bc3c88f31e4b0c
eddiebee <edbeeotudor@gmail.com>
2023-03-10 10:44:19 +0000

    Accept/utilize children props in Tables component

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -9,7 +9,7 @@ import convert from 'ethereum-unit-converter'
 const transactionApi = 'https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0x5a79be6cdce26bc853d72919bf98a0378641b607&address=0x000000000000000000000000000000000000dEaD&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=RM1PBIHRH9F5JVJYH7ENZI6SWZMGXU3FTY';
 
 
-const Tables = ({tableClass, tableTitle, pagination}) => {
+const Tables = ({tableClass, tableTitle, pagination, children}) => {
     const [tableData, setTableData] = useState()
 
     const columns = [
@@ -85,6 +85,9 @@ const Tables = ({tableClass, tableTitle, pagination}) => {
                         responsive
                     />
                 </div>
+            </div>
+            <div className='tables-children'>
+                {children}
             </div>
         </div>
     );
