d189f0469738ca925bc3a262fe9948e4d26aa2e5
eddiebee <edbeeotudor@gmail.com>
2023-03-12 00:32:47 +0000

    Implement Tables with child Link component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,30 +5,28 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 // import '../pages/BurnHistory'
 
-
-
-
-
 const Dashboard = () => {
 
     return (
         <div className='dashboard-main'>
             <h1>Burn & Supply</h1>
             <DisplayBoxes />
-            <Tables 
-                tableTitle="Latest History"
-            />
-            <Link to="../BurnHistory" className='view-all-btn'>View All</Link>
-            <Tables 
-                tableTitle="Burn Ranking"
-                
-     
-            />
-            <Link to="../TopBurners" className='view-all-btn'>View All</Link>
-            
-        </div>
+            <div>
+                <Tables
+                    tableTitle="Latest History"
 
-        
+                >
+                    <Link to="../BurnHistory" className='view-all-btn'>View All</Link>
+                </Tables>
+            </div>
+            <div>
+                <Tables
+                    tableTitle="Burn Ranking"
+                >
+                    <Link to="../TopBurners" className='view-all-btn'>View All</Link>
+                </Tables>
+            </div>
+        </div>
     );
 };
 
