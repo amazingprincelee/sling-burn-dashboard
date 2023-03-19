d8a1f301b7fcd1c4ba27a4809afbb18ba2f0608a
eddiebee <edbeeotudor@gmail.com>
2023-03-19 02:18:28 +0000

    Refactor and format reviewsBar component

--- src/components/progressbar/reviewsBar.js
+++ src/components/progressbar/reviewsBar.js
@@ -1,56 +1,50 @@
-import React from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-import RewiewsProvider from './reviewsProvider';
-
+import React from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import RewiewsProvider from "./reviewsProvider";
 
 const RewiewsBar = (props) => {
+  const { score } = props;
 
-    const {score} = props;
+  // function for calculating the progressbar color
+  const calcColor = (percent, start, end) => {
+    let a = percent / 100,
+      b = (end - start) * a,
+      c = b + start;
 
-// function for calculating the progressbar color
-    const calcColor = (percent, start, end) => {
-        let a = percent/100,
-        b = (end - start) * a,
-        c = b + start;
+    return "hsl(" + c + ", 100%, 50%)";
+  };
 
-        return "hsl(" + c + ", 100%, 50%)";
-    };
-
-    return (
-       <RewiewsProvider valueStart={0} valueEnd={score}>
-            {(value) => (
-        <CircularProgressbar 
-        value = {value}
-        text = {`${value} %`}
-        circleRatio = {0.7}
-        styles = {{
+  return (
+    <RewiewsProvider valueStart={0} valueEnd={score}>
+      {(value) => (
+        <CircularProgressbar
+          value={value}
+          text={`${value} %`}
+          circleRatio={0.7}
+          styles={{
             trail: {
-                strokeLinecap: "butt",
-                transform: "rotate(-126deg)",
-                transformOrigin: "center center"
-                
+              strokeLinecap: "butt",
+              transform: "rotate(-126deg)",
+              transformOrigin: "center center",
             },
 
             path: {
-                strokeLinecap: "butt",
-                transform: "rotate(-126deg)",
-                transformOrigin: "center center",
-                stroke: calcColor(value, 0, 120)
+              strokeLinecap: "butt",
+              transform: "rotate(-126deg)",
+              transformOrigin: "center center",
+              stroke: calcColor(value, 0, 120),
             },
 
             text: {
-                fill: "#808080"
-            }
-        }}
-
-        strokeWidth = {10}
-
-       />
-            )}
-       </RewiewsProvider>
-    );
+              fill: "#808080",
+            },
+          }}
+          strokeWidth={10}
+        />
+      )}
+    </RewiewsProvider>
+  );
 };
-
 
 export default RewiewsBar;
