4247d0ebd634ea810e31f3ab82ccdf986473089a
eddiebee <edbeeotudor@gmail.com>
2023-03-19 02:19:32 +0000

    Refactor App component to setup improved UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Dashboard from './pages/Dashboard.jsx';
-import TopBurners from './pages/TopBurners';
-import BurnReward from './pages/BurnReward';
-import BurnHistory from './pages/BurnHistory';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard.jsx";
+import TopBurners from "./pages/TopBurners";
+import BurnReward from "./pages/BurnReward";
+import BurnHistory from "./pages/BurnHistory";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Sidebar>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/burnhistory" element={<BurnHistory />} />
-          <Route path="/topburners" element={<TopBurners />} />
-          <Route path="/reward" element={<BurnReward />} />
-        </Routes>
-      </Sidebar>
-    </BrowserRouter>
+    <div className="home-grid">
+      <BrowserRouter>
+        <Sidebar />
+        <div className="routes">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/burnhistory" element={<BurnHistory />} />
+            <Route path="/topburners" element={<TopBurners />} />
+            <Route path="/reward" element={<BurnReward />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
