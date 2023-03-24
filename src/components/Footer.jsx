9c6e1a59a25c0a9f41cc084add656d949577cc12
prince Lee <princeleepraise@gmail.com>
2023-03-24 11:11:00 +0100

    adjusted footer element on desktop view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,41 @@
-import React from 'react';
+import React from "react";
 import { FaTwitter, FaTelegram } from "react-icons/fa";
-import logo from "../logo.png"
-
+import logo from "../logo.png";
 
 const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div>
+          <img className="f-logo" src={logo} alt="logo" />
+          <p className="f-text">
+            The Sling Burn Project. <br />1 token sells trigger the burn
+            mechanism hourly.{" "}
+          </p>
+        </div>
 
-    return (
-        
-            <div className='footer-content'>
+        <div className="f-text">
+          <h4>Utilities</h4>
+          <ul>
+            <li>Staking</li>
+            <li>Burn Dashboard</li>
+          </ul>
+        </div>
 
-                <div>
-                    <img className='f-logo' src={logo} alt="logo" />
-                    <p className='f-text'>The Sling Burn Project. <br/>1 token sells trigger the burn mechanism hourly.  </p>
-                    
-
-                </div>
-
-                <div className='f-text'>
-                    <h4>Utilities</h4>
-                    <ul>
-                    <li>Staking</li>
-                    <li>Burn Dashboard</li>
-                    </ul>
-                </div>
-
-                <div className='f-text'>
-                    <h4>Information</h4>
-                    <div className='social'>
-                    <a href="http://t.me/slingclassic"><FaTelegram size={35}/></a>   
-                    <a href="https://twitter.com/SlingClassic"><FaTwitter size={35} /></a>     
-                    </div>
-                </div>
-
-            </div>
-            
-      
-    
-    );
+        <div className="f-text">
+          <h4>Information</h4>
+          <div className="social">
+            <a href="http://t.me/slingclassic">
+              <FaTelegram size={35} />
+            </a>
+            <a href="https://twitter.com/SlingClassic">
+              <FaTwitter size={35} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
-
 
 export default Footer;
